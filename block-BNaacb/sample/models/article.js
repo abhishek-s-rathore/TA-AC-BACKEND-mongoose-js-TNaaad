d20450512b1e774c13ef2c393e78a2b8794788d0
Abhishek <abhishek.990914@gmail.com>
2021-08-17 14:32:47 +0530

    block Submitted

---
+++ block-BNaacb/sample/models/article.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+var articleSCheema = new Scheema({
+  name: String,
+  body: String,
+  author: String,
+  readMinutes: Number,
+});
